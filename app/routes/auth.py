96d6396385f6eca6cc1714ff09dc5df8877e352c
ravindra-levis <ravindrasudha8@gmail.com>
2026-05-07 12:00:46 +0530

    fix: added routes folder to include auth file

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,7 +1,7 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from jose import jwt
 from passlib.context import CryptContext
-from .config import SECRET_KEY, ALGORITHM, TOKEN_EXPIRE_MINUTES
+from ..config import SECRET_KEY, ALGORITHM, TOKEN_EXPIRE_MINUTES
 
 pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
@@ -13,6 +13,6 @@ def verify_password(plain_password, hashed_password):
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"exp": expire})
+    expire = datetime.now(timezone.utc) + timedelta(minutes=TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire, "type": "access"})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM) # type: ignore
